c9c9823f8582c74d12ef8bb8a5e0d0a890ccfa71
Konstantinos <kwstas654321@gmail.com>
2021-03-05 00:32:15 +0200

    Update UserRepository.java

--- Jwt/src/main/java/com/spring/boot/repository/UserRepository.java
+++ Jwt/src/main/java/com/spring/boot/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.spring.boot.repository;
 
 import com.spring.boot.entities.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    public User findByEmail(String username);
-
+    Optional<User> findByUsername(String username);
 }
