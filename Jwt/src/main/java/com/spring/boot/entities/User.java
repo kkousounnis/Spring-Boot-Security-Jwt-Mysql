42b0e0e7f2fb13bd2129c73527226db757f60d12
Konstantinos <kwstas654321@gmail.com>
2021-03-04 19:33:06 +0200

    Update User.java

--- Jwt/src/main/java/com/spring/boot/entities/User.java
+++ Jwt/src/main/java/com/spring/boot/entities/User.java
@@ -11,13 +11,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany; 
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-public class User implements Serializable {
+public class User implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
