336620a52f85320f7a28c30fdd1409abbb09b09b
Konstantinos <kwstas654321@gmail.com>
2021-03-04 19:32:54 +0200

    Update SecutiryConfig.java

--- Jwt/src/main/java/com/spring/boot/configuration/SecutiryConfig.java
+++ Jwt/src/main/java/com/spring/boot/configuration/SecutiryConfig.java
@@ -1,14 +1,34 @@
 package com.spring.boot.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(
+  prePostEnabled = true, 
+  securedEnabled = true, 
+  jsr250Enabled = true)
+@EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private final UserDetailsService userDetailsService;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecutiryConfig(UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -17,12 +37,10 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/hello")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().httpBasic();
+        http.csrf()
+                .ignoringAntMatchers("/**")
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()));
     }
 }
