e669404fc7f5cfbbbb272b5479f43bf3b6b37fe6
Konstantinos <kwstas654321@gmail.com>
2021-03-04 18:36:03 +0200

    Create UserServiceImpl.java

---
+++ Jwt/src/main/java/com/spring/boot/services/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.spring.boot.services;
+
+import com.spring.boot.entities.Role;
+import com.spring.boot.entities.User;
+import com.spring.boot.repository.UserRepository;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+            //return null;
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+    }
+}
