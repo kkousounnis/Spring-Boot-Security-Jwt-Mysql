233a19c2fc3d48db6103d98017965f9821460615
Konstantinos <kwstas654321@gmail.com>
2021-03-05 00:38:45 +0200

    Update WebSecutiryConfig.java

--- Jwt/src/main/java/com/spring/boot/config/WebSecutiryConfig.java
+++ Jwt/src/main/java/com/spring/boot/config/WebSecutiryConfig.java
@@ -1,15 +1,18 @@
 package com.spring.boot.config;
 
+import com.spring.boot.config.security.jwt.AuthEntryPointJwt;
+import com.spring.boot.config.security.jwt.AuthTokenFilter;
+import com.spring.boot.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,16 +26,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecutiryConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private final UserDetailsServiceImpl userDetailsService;
-    
-    @Autowired
-    private final PasswordEncoder passwordEncoder;
+    UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    public WebSecutiryConfig(UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-        this.userDetailsService = userDetailsService;
+    AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
     }
 
     @Bean
@@ -43,6 +44,12 @@ public class WebSecutiryConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
