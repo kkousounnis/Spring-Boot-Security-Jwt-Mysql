f644f3efe578a7e39006e610d5072bf69bcf170e
Konstantinos <kwstas654321@gmail.com>
2021-03-04 16:14:30 +0200

    Update SecutiryConfig.java

--- Jwt/src/main/java/com/spring/boot/configuration/SecutiryConfig.java
+++ Jwt/src/main/java/com/spring/boot/configuration/SecutiryConfig.java
@@ -1,9 +1,28 @@
 package com.spring.boot.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecutiryConfig extends WebSecurityConfigurerAdapter{
+public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return (new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/hello")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().httpBasic();
+    }
 }
