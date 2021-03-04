b5babe9672d132a0f0c9ac31416a00d7e92aa0b8
Konstantinos <kwstas654321@gmail.com>
2021-03-04 15:19:13 +0200

    Create SecutiryConfig.java

---
+++ Jwt/src/main/java/com/spring/boot/configuration/SecutiryConfig.java
@@ -0,0 +1,9 @@
+package com.spring.boot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecutiryConfig extends WebSecurityConfigurerAdapter{
+
+}
