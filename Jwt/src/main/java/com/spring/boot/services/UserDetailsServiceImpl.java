bb1207d29de6e89722b5011f377e702e4d0dfc27
Konstantinos <kwstas654321@gmail.com>
2021-03-06 19:38:49 +0200

    Update UserDetailsServiceImpl.java

--- Jwt/src/main/java/com/spring/boot/services/UserDetailsServiceImpl.java
+++ Jwt/src/main/java/com/spring/boot/services/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.spring.boot.services;
 
 import com.spring.boot.entities.User;
-import com.spring.boot.entities.UserDetailsImpl;
 import com.spring.boot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,19 +9,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
 
-    @Autowired
-    UserRepository userRepository;
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
-
-        return UserDetailsImpl.build(user);
-    }
+		return UserDetailsImpl.build(user);
+	}
 
 }
