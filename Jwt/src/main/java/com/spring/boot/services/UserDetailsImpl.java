d2b749b84bbd57b9d451039cca8ca933a01db4b9
Konstantinos <kwstas654321@gmail.com>
2021-03-07 15:22:52 +0200

    Update UserDetailsImpl.java

--- Jwt/src/main/java/com/spring/boot/services/UserDetailsImpl.java
+++ Jwt/src/main/java/com/spring/boot/services/UserDetailsImpl.java
@@ -9,88 +9,90 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore; 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.spring.boot.entities.User;
 
 public class UserDetailsImpl implements UserDetails {
-	private static final long serialVersionUID = 1L;
 
-	private Integer id;
+    private static final long serialVersionUID = 1L;
 
-	private String email;
+    private Integer id;
 
-	@JsonIgnore
-	private String password;
+    private String email;
 
-	private Collection<? extends GrantedAuthority> authorities;
+    @JsonIgnore
+    private String password;
 
-	public UserDetailsImpl(Integer id, String email, String password,
-			Collection<? extends GrantedAuthority> authorities) {
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.authorities = authorities;
-	}
+    private Collection<? extends GrantedAuthority> authorities;
 
-	public static UserDetailsImpl build(User user) {
-		List<GrantedAuthority> authorities = user.getRoles().stream()
-				.map(role -> new SimpleGrantedAuthority(role.getRole().name()))
-				.collect(Collectors.toList());
+    public UserDetailsImpl(Integer id, String email, String password,
+            Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
 
-		return new UserDetailsImpl(
-				user.getId(), 
-				user.getEmail(),
-				user.getPassword(), 
-				authorities);
-	}
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toList());
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return authorities;
-	}
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
 
-	public String getUsername() {
-		return email;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	@Override
-	public String getPassword() {
-		return password;
-	}
+    public String getUsername() {
+        return email;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		UserDetailsImpl user = (UserDetailsImpl) o;
-		return Objects.equals(id, user.id);
-	}
-  
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+
 }
